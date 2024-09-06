e65b3a5d2c4ea6749daf517d5ccddbcfa570b62f
vanthiyadhevan <samvickey96@gmail.com>
2024-09-06 17:55:57 +0530

    added Variables_01

--- first_instance.tf
+++ first_instance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  # secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.ACCESS_KEY_ID
+  secret_key = var.SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "intro" {
