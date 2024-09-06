50fde94b0485ef5fbc4e45f45ed2c46a7f7c32b7
vanthiyadhevan <samvickey96@gmail.com>
2024-09-06 15:29:45 +0530

    initial_commit

---
+++ first_instance.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+  # access_key = "<your_access_key>"
+  # secret_key = "<your_secret_key>"
+}
+
+resource "aws_instance" "intro" {
+  ami                    = "ami-0e86e20dae9224db8"
+  instance_type          = "t2.small"
+  key_name               = "vickey-prod-key"
+  vpc_security_group_ids = ["sg-096add7865a855a64"]
+
+  # Specify the root volume for the instance
+  root_block_device {
+    volume_size = 20    
+    volume_type = "gp3" 
+  }
+
+  # User data to run scripts at instance launch
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update -y
+    # Install Docker
+    sudo apt-get install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+
+    # Add Jenkins user to Docker group
+    sudo usermod -aG docker jenkins
+    sudo chmod 666 /var/run/docker.sock
+  EOF
+
+  tags = {
+    Name = "jenkins_and_docker"
+  }
+}
+
+# Outputs for Public and Private IP addresses
+output "PublicIP" {
+  value = aws_instance.intro.public_ip
+}
+
+output "PrivateIP" {
+  value = aws_instance.intro.private_ip
+}
